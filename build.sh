757b7e1da77784b719d8ed35a873c4611c2561fa
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2022-07-20 16:05:29 +0100

    PP-9722: Add dev to the list of accounts with no ec2 instances

--- build.sh
+++ build.sh
@@ -2,15 +2,22 @@
 
 set -eo pipefail
 
-VERSION="0.4.0"
+VERSION="0.5.0"
 
 IFS=$'\n\t'
 
 if [ -z "$1" ]; then
+  echo "Build and upload the compliance lambda to S3 for later deployment"
+  echo
   echo "Usage: $0 <S3_BUCKET> [S3_KEY_PREFIX]"
   echo
   echo "  S3_BUCKET is usually either 'pay-govuk-lambda-deploy' or 'pay-govuk-lambda-ci'"
   echo "  S3_KEY_PREFIX defaults to pay-aws-compliance and will be suffixed with the version number"
+  echo
+  echo "Lambdas in dev, test, and ci are deployed from pay-govuk-lambda-ci"
+  echo "Lambdas in deploy, staging, and production are deployed from pay-govuk-lambda-deploy"
+  echo "If you wish to deploy a new version you need to run this build twice, once with each of those S3_BUCKET values"
+  echo
   exit 1
 fi
 
